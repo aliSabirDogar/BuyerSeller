ab20809318d741a7d571fcd8a1220a0c496717e7
Personal <saqibaliriaz888@gmail.com>
2020-11-10 21:29:59 +0500

    saqib  commit

--- app/src/main/java/com/radiocodeford/buyerseller/ClientRegisteration.java
+++ app/src/main/java/com/radiocodeford/buyerseller/ClientRegisteration.java
@@ -2,6 +2,7 @@ package com.radiocodeford.buyerseller;
 
 import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
 import android.view.View.OnClickListener;
@@ -26,12 +27,15 @@ import java.util.Map;
 
 public class ClientRegisteration extends AppCompatActivity {
     Button signup;
+    SharedPreferences pref;
+    SharedPreferences.Editor editor;
     private RequestQueue requestQueue;
-    Context mContext=null;
+    Context mContext = null;
     String URL = "http://tuscomprasfacil.com/mpew/mpew/restapi/index.php/signup_client";
-    String strpassword,strConfirmPassword;
-
-    EditText name, surname,address,district,province,department,mail,password,phone,confirmPassword;
+    String URL_insert_player_id = "http://tuscomprasfacil.com/mpew/mpew/restapi/index.php/insert_player_id_client";
+    String strpassword, strConfirmPassword;
+    String player_id;
+    EditText name, surname, address, district, province, department, mail, password, phone, confirmPassword;
 
     /* Access modifiers changed, original: protected */
     public void onCreate(Bundle savedInstanceState) {
@@ -39,20 +43,23 @@ public class ClientRegisteration extends AppCompatActivity {
         setContentView(R.layout.activity_client_registeration);
         mContext = getApplicationContext();
         requestQueue = Volley.newRequestQueue(this);
-        if(mContext!=null) {
+        if (mContext != null) {
             requestQueue = Volley.newRequestQueue(mContext);
         }
         name = (EditText) findViewById(R.id.tf_clientRegisteration_name);
         surname = (EditText) findViewById(R.id.tf_clientRegisteration_surName);
-        phone= (EditText) findViewById(R.id.tf_clientRegisteration_cellular);
+        phone = (EditText) findViewById(R.id.tf_clientRegisteration_cellular);
         address = (EditText) findViewById(R.id.tf_clientRegisteration_address);
         province = (EditText) findViewById(R.id.tf_clientRegisteration_province);
         department = (EditText) findViewById(R.id.tf_clientRegisteration_department);
-        mail= (EditText) findViewById(R.id.tf_clientRegisteration_mail);
+        mail = (EditText) findViewById(R.id.tf_clientRegisteration_mail);
         district = (EditText) findViewById(R.id.tf_clientRegisteration_district);
 
         password = (EditText) findViewById(R.id.tf_clientRegisteration_password);
         confirmPassword = (EditText) findViewById(R.id.tf_clientRegisteration_confirmPassword);
+
+        pref = this.getSharedPreferences("buyerSeller", Context.MODE_PRIVATE);
+        player_id = pref.getString("player_id", player_id);
         this.signup = (Button) findViewById(R.id.btn_client_SignUp);
         this.signup.setOnClickListener(new OnClickListener() {
             public void onClick(View view) {
@@ -63,6 +70,7 @@ public class ClientRegisteration extends AppCompatActivity {
                         @Override
                         public void onResponse(String response) {
                             //pDialog.dismiss();
+                            PostPlayerid();
                             Toast.makeText(ClientRegisteration.this, "Registered Successfully as a client", Toast.LENGTH_LONG).show();
                         }
                     }, new Response.ErrorListener() {
@@ -85,25 +93,50 @@ public class ClientRegisteration extends AppCompatActivity {
                             params.put("province", province.getText().toString());
                             params.put("department", department.getText().toString());
                             params.put("mail", mail.getText().toString());
-
                             params.put("district", district.getText().toString());
                             params.put("password", password.getText().toString());
                             return params;
                         }
                     };
-
-
                     requestQueue.add(stringRequest);
-
                     ClientRegisteration.this.startActivity(new Intent(ClientRegisteration.this, LoginSellerBuyer.class));
-
-                }
-                else
-                {
+                } else {
                     Toast.makeText(ClientRegisteration.this, "Confirm Password should be same ", Toast.LENGTH_LONG).show();
                 }
             }
 
+
         });
     }
+
+    //onesignal playerid insert
+    public void PostPlayerid() {
+        StringRequest stringRequest = new StringRequest(Request.Method.POST, URL_insert_player_id, new Response.Listener<String>() {
+            @Override
+            public void onResponse(String response) {
+                //pDialog.dismiss();
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Toast.makeText(ClientRegisteration.this, "Try again later", Toast.LENGTH_LONG).show();
+                Toast.makeText(ClientRegisteration.this, error.toString(), Toast.LENGTH_LONG).show();
+            }
+        }) {
+            @Override
+            protected Map<String, String> getParams() throws AuthFailureError {
+
+                // Creating Map String Params.
+                Map<String, String> params = new HashMap<>();
+
+                params.put("cell", phone.getText().toString());
+                params.put("player_id", player_id);
+
+                return params;
+            }
+        };
+        requestQueue.add(stringRequest);
+    }
+
 }
+
